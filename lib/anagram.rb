8e6dacc955ca768d9bef54979afa8c0c10dbf70c
Danny Soto <87dsoto@gmail.com>
2020-01-28 04:22:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,6 @@ class Anagram
   end 
   
   def match(array)
-    array.keep_if do |word|
-      @word.split("").sort == word.split("").sort
-    end 
+    array.keep_if {|word| @word.split("").sort == word.split("").sort}
   end 
 end 
