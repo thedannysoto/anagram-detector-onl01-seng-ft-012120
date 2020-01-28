cdc8b417d7b3f69d02b1e2ae3fb1379c15cab731
Danny Soto <87dsoto@gmail.com>
2020-01-28 04:23:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+    array.keep_if {|word| @word.split("").sort == word.split("").sort}
+  end 
+end 
